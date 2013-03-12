d67b26bf0828c575e4d66a2c2b3b74ed931bce96
Azkar <sdm350@gmail.com>
2013-03-11 21:55:43 -0400

    Changed server and client to use UDP connection

--- Server/Server/Server.ino
+++ Server/Server/Server.ino
@@ -7,34 +7,43 @@
 
 #include <Ethernet.h>
 #include <SPI.h>
+#include <EthernetUdp.h>
 
 //mac address for my Arduino
 byte mac[] = { 0x00, 0xAA, 0xBA, 0xC4, 0xFF, 0x21 };
-IPAddress ip(192, 168, 0, 222);
+IPAddress ip(10, 0, 0, 66);
 
-EthernetServer server = EthernetServer(23);
+int PORT = 8771;
+
+byte remoteIp[4];
+unsigned int remotePort;
+byte packetBuffer[UDP_TX_PACKET_MAX_SIZE];
+char replyBuffer[] = "ack";
+
+EthernetUDP udp;
 
 void setup()
 {
   Ethernet.begin(mac, ip);
+  udp.begin(PORT);
   
   Serial.begin(9600);
   
   //Starts listening for clients on port 23
-  server.begin();
+  
 }
 
 void loop()
 {
-  EthernetClient client = server.available();
-  if (client == true) {
-    //command size limited to 12 right now
-    char command[13];
-    for (int i=0; i < 13; ++i)
-    {
-      command[i] = client.read(); 
-    }
-    command[12] = 0;
-    Serial.println(command[0]);
-  }  
+   int packetSize = udp.parsePacket();
+   
+   if (packetSize)
+   {
+     packetSize = packetSize - 8; //header
+     
+     udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+     Serial.println((char *)packetBuffer);
+   }     
+   delay(10);
 }
+
--- python/DrinkClient.py
+++ python/DrinkClient.py
@@ -1,11 +1,12 @@
 # Echo client program
 import socket
 
-HOST = '192.168.0.222'    # The remote host
-PORT = 23                 # The same port as used by the server
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+HOST = '10.0.0.66'    # The remote host
+PORT = 8771           # The same port as used by the server.
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect((HOST, PORT))
-s.sendall('Hello, world')
+s.sendall('Hello World!')
 #data = s.recv(1024)
 s.close()
 #print 'Received', repr(data)
