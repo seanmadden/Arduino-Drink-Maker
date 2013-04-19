0af46df69dbfe8c2082af8fcf21fc35a8f142a92
Azkar <sdm350@gmail.com>
2013-04-19 06:32:59 -0400

    Added templates to django site

--- Site/Drinkbot/models.py
+++ Site/Drinkbot/models.py
@@ -7,8 +7,14 @@ class Ingredient(models.Model):
     Id = models.PositiveIntegerField(primary_key='true')
     name = models.CharField(max_length='100')
 
+    def __unicode__(self):
+        return self.name
+
 
 class Drink(models.Model):
     Id = models.PositiveIntegerField(primary_key='true')
     name = models.CharField(max_length='50')
     ingredients = models.ManyToManyField(Ingredient)
+
+    def __unicode__(self):
+        return self.name
--- Site/Drinkbot/views.py
+++ Site/Drinkbot/views.py
@@ -1,22 +1,16 @@
 # Create your views here.
-from django.http import HttpResponse
-from django.shortcuts import render_to_response
+from django.shortcuts import render, get_object_or_404
 from Drinkbot.models import Drink
 
 
 def index(request):
-    return HttpResponse("Hello world. Let's Drink!")
+    drinkList = Drink.objects.all()
+    context = {'drinkList': drinkList}
+    return render(request, 'Drinkbot/index.html', context)
 
 
 def detail(request, drink_id):
-    drink = Drink.objects.get(pk=drink_id)
+    drink = get_object_or_404(Drink, pk=drink_id)
     ingList = list(drink.ingredients.all())
-    ings = ', '.join([ing.name for ing in ingList])
-    return HttpResponse("You are looking at beverage: %s which has ingredients: %s" % (drink.name, ings))
-
-
-def view_page(request, page_name):
-    try:
-        page = Drink.objects.get(pk=page_name)
-    except Drink.DoesNotExist:
-        return render_to_response('create.htm', {'page_name', page_name})
+    return render(request, 'Drinkbot/detail.html', {'drink': drink,
+                                                    'inglist': ingList})
